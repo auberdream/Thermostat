0ff6ce3a0825d4464191365d77f4e3deda59847a
Ellie <elliewem@gmail.com>
2018-02-01 17:26:51 +0000

    Refactor to use private methods

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -6,17 +6,18 @@ function Thermostat() {
 const MAXTEMP = 32;
 const MINTEMP = 10;
 const PSMAXTEMP = 25;
+const MIN_LOW_USAGE = 18;
+const MAX_MED_USAGE = 25;
 
 Thermostat.prototype.reset = function() {
   this.temperature = 20
 };
 
-
 Thermostat.prototype.up = function() {
-  if (this.temperature + 1 > MAXTEMP) {
+  if (this._isOverMaxTemp()) {
     throw new Error(`maximum temperature is ${MAXTEMP}`)
   }
-  if (this.isPowersaving && (this.temperature + 1 > PSMAXTEMP)) {
+  if (this._isOverPSMaxTemp()) {
     throw new Error(`maximum temperature is ${PSMAXTEMP} in powersave mode`)
   }
     ++this.temperature
@@ -33,16 +34,32 @@ Thermostat.prototype.modeSwitch = function() {
   !this.isPowersaving ? this.isPowersaving = true : this.isPowersaving = false;
 };
 
-Thermostat.prototype._isUnderMinTemp = function() {
-  return ((this.temperature - 1) < MINTEMP)
-};
-
 Thermostat.prototype.usage = function() {
-  if (this.temperature < 18) {
+  if (this._isLowUsage()) {
     return("low-usage")
   }
-  if (this.temperature >= 18 && this.temperature < 25) {
+  if (this._isMediumUsage()) {
     return("medium-usage")
   }
   return("high-usage")
+};
+
+Thermostat.prototype._isUnderMinTemp = function() {
+  return ((this.temperature - 1) < MINTEMP);
+};
+
+Thermostat.prototype._isOverMaxTemp = function() {
+  return (this.temperature + 1 > MAXTEMP);
+};
+
+Thermostat.prototype._isOverPSMaxTemp = function() {
+  return this.isPowersaving && (this.temperature + 1 > PSMAXTEMP);
+};
+
+Thermostat.prototype._isLowUsage = function() {
+  return (this.temperature < MIN_LOW_USAGE);
+};
+
+Thermostat.prototype._isMediumUsage = function() {
+  return (this.temperature >= MIN_LOW_USAGE && this.temperature < MAX_MED_USAGE)
 };
